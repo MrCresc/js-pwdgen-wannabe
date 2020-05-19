4ee821c8254ffa309bd3e6604a74db32c8e76fdc
Crescenzo De Martino <cresckamina@gmail.com>
2020-05-19 15:39:37 +0200

    Primo Upload password generator

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var nomeUtente = prompt('Inserisci il tuo nome')
+
+var cognomeUtente = prompt('Inserisci il tuo cognome')
+
+var colorePreferito = prompt('Inserisci il tuo colore preferito')
+
+document.getElementById('hola').innerHTML = nomeUtente + cognomeUtente + colorePreferito + 19
